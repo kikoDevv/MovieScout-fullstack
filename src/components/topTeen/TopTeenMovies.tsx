5e2183d5916ada72f55194c0a8b307b05cd43823
kikoDevv <kiko.devv@gmail.com>
2025-07-29 18:37:20 +0200

    fixed path issue

--- src/components/topTeen/TopTeenMovies.tsx
+++ src/components/topTeen/TopTeenMovies.tsx
@@ -4,30 +4,7 @@ import MovieSlider from "../UI/MovieSlider";
 import { getTopTeen } from "./topTeenApi";
 import { useQuery } from "@tanstack/react-query";
 
-export default function TopTeenMovies() {
-  const movies = [
-    { id: 1, title: "Oppenheimer", poster: "/movieImages/1.jpeg" },
-    { id: 2, title: "Barbie", poster: "/movieImages/2.jpeg" },
-    { id: 3, title: "Dune", poster: "/movieImages/3.jpeg" },
-    { id: 4, title: "John Wick 4", poster: "/movieImages/4.jpeg" },
-    { id: 5, title: "Avatar 2", poster: "/movieImages/5.jpeg" },
-    { id: 6, title: "Avatar 3", poster: "/movieImages/6.jpeg" },
-    { id: 7, title: "Avatar 4", poster: "/movieImages/7.jpeg" },
-    { id: 8, title: "Avatar 4", poster: "/movieImages/8.jpeg" },
-    { id: 9, title: "Avatar 4", poster: "/movieImages/4.jpeg" },
-    { id: 10, title: "Avatar 4", poster: "/movieImages/7.jpeg" },
-    { id: 11, title: "Avatar 4", poster: "/movieImages/8.jpeg" },
-    { id: 12, title: "Avatar 4", poster: "/movieImages/2.jpeg" },
-    { id: 13, title: "Avatar 4", poster: "/movieImages/3.jpeg" },
-    { id: 14, title: "Avatar 4", poster: "/movieImages/4.jpeg" },
-    { id: 15, title: "Avatar 4", poster: "/movieImages/5.jpeg" },
-    { id: 16, title: "Avatar 4", poster: "/movieImages/6.jpeg" },
-    { id: 17, title: "Avatar 4", poster: "/movieImages/7.jpeg" },
-    { id: 18, title: "Avatar 4", poster: "/movieImages/8.jpeg" },
-    { id: 19, title: "Avatar 4", poster: "/movieImages/8.jpeg" },
-    { id: 10, title: "Avatar 4", poster: "/movieImages/8.jpeg" },
-  ];
-
+export default function TopTeen() {
   const {
     data: movieData,
     isLoading,
